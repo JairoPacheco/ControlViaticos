1a4b31f91317de13264a67f7707778ccd45bd932
Fran Soto Q <fran-soto@hotmail.es>
2020-06-11 19:11:52 -0600

    sp: addViatico

--- DataBaseScripts/StoredProcedures.sql
+++ DataBaseScripts/StoredProcedures.sql
@@ -59,10 +59,65 @@ BEGIN
 	INSERT INTO Evento(fecha, trabajo, tieneContrato, duracion, problemaReportado, problemaResuelto,
 						idSucursal, codigoCentroCosto, idLabor, idTipoSoporte, idMotivo)
 	VALUES (@fecha, @trabajo, @tieneContrato, @duracion, @problemaReportado, @problemaResuelto,
-			@idSucursal, @tieneContrato, @duracion, @problemaReportado, @problemaResuelto);
+			@idSucursal, @codigoCentroCosto, @idLabor, @idTipoSoporte, @idMotivo);
 
 	-- Operación realizada exitosamente
 	SET @status = 0;
 	SELECT @status AS statusCode;
 END
 GO
+
+CREATE OR ALTER PROC addViatico
+    @fecha             TFecha,
+    @factura           VARCHAR(50),
+    @monto             TMonto,
+    @numPagos          INT,
+    @notas             VARCHAR(100),
+    @boleta            TBoleta,
+    @idTipoViatico     INT,
+    @idProveedor       INT,
+    @idResponsable     INT,
+    @idEvento          INT,
+    @status            TINYINT OUTPUT
+AS
+BEGIN
+    -- Si no existe el tipo viatico (código 1)
+    IF NOT EXISTS (SELECT id FROM TipoViatico WHERE id = @idTipoViatico)
+    BEGIN
+        SET @status = 1;
+        SELECT @status AS statusCode;
+        RETURN;
+    END
+
+	-- Si no existe el proveedor (código 2)
+    IF NOT EXISTS (SELECT id FROM Proveedor WHERE id = @idProveedor)
+    BEGIN
+        SET @status = 2;
+        SELECT @status AS statusCode;
+        RETURN;
+    END
+
+	-- Si no existe el proveedor (código 3)
+    IF NOT EXISTS (SELECT id FROM Recurso WHERE id = @idResponsable)
+    BEGIN
+        SET @status = 3;
+        SELECT @status AS statusCode;
+        RETURN;
+    END
+
+	-- Si no existe el evento (código 4)
+    IF NOT EXISTS (SELECT id FROM Evento WHERE id = @idEvento)
+    BEGIN
+        SET @status = 4;
+        SELECT @status AS statusCode;
+        RETURN;
+    END
+
+    INSERT INTO Viatico(fecha, factura, monto, numPagos, notas, boleta, idTipoViatico, idProveedor, idResponsable, idEvento)
+    VALUES (@fecha, @factura, @monto, @numPagos, @notas, @boleta, @idTipoViatico, @idProveedor, @idResponsable, @idEvento)
+
+    SET @status = 0;
+    SELECT @status AS statusCode;
+    RETURN;
+
+END

c1b56c36b5a12595af3a6a6bff32922d3739edc5
Sebastian <sebasrv26@estudiantec.cr>
2020-06-22 14:18:41 -0600

    Cursors and Triggers

--- DataBaseScripts/Cursors.sql
+++ DataBaseScripts/Cursors.sql
@@ -1,3 +1,50 @@
 USE ControlViaticos;
 GO
 
+-- 1. cursor que imprime el cliente y sus sucursales
+PRINT 'Cursor 1'
+DECLARE cursor_cliente CURSOR FOR 
+SELECT codigo, razonComercial FROM Cliente
+DECLARE @codigo TCodCliente
+DECLARE @razonC VARCHAR(30)
+OPEN cursor_cliente --Abrimos el cursor para iniciar el recorrido del mismo
+FETCH NEXT FROM cursor_cliente INTO @codigo, @razonC
+WHILE @@fetch_status = 0
+BEGIN
+	PRINT 'La razón comercial es ' + @razonC
+    DECLARE cursor_sucursal CURSOR FOR
+    SELECT sucursal FROM Sucursal WHERE codigoCliente = @codigo
+    DECLARE @sucursal VARCHAR(20)
+    OPEN cursor_sucursal
+    FETCH NEXT FROM cursor_sucursal INTO @sucursal
+    WHILE @@fetch_status = 0
+    BEGIN
+        PRINT '    La sucursal es ' + @sucursal
+        FETCH NEXT FROM cursor_sucursal INTO @sucursal
+    END
+	PRINT'---------------------------------------------'
+    CLOSE cursor_sucursal --Cierra el cursor.
+    DEALLOCATE cursor_sucursal
+	FETCH NEXT FROM cursor_cliente into @codigo, @razonC
+END
+CLOSE cursor_cliente --Cierra el cursor.
+DEALLOCATE cursor_cliente --Lo libera de la memoria y lo destruye.
+GO
+
+PRINT ''
+PRINT 'Cursor 2'
+-- 2. cursor que imprime el username y nombre de los recursos (responsables)
+DECLARE cursor_recurso CURSOR FOR 
+SELECT responsable, descripción FROM Recurso
+DECLARE @responsable VARCHAR(20)
+DECLARE @descripción VARCHAR(100)
+OPEN cursor_recurso --Abrimos el cursor para iniciar el recorrido del mismo
+FETCH NEXT FROM cursor_recurso INTO @responsable, @descripción
+WHILE @@fetch_status = 0
+BEGIN
+	PRINT 'Username: ' + @responsable + '    Nombre: ' + @descripción
+	FETCH NEXT FROM cursor_recurso INTO @responsable, @descripción
+END
+CLOSE cursor_recurso --Cierra el cursor.
+DEALLOCATE cursor_recurso --Lo libera de la memoria y lo destruye.
+GO
--- DataBaseScripts/Triggers.sql
+++ DataBaseScripts/Triggers.sql
@@ -1,3 +1,42 @@
 USE ControlViaticos;
 GO
 
+-- 1. Trigger que calcula el monto del viatico para el kilometraje
+CREATE TRIGGER triggerKilometraje ON Kilometraje
+AFTER INSERT 
+AS
+	DECLARE @idViatico INT
+	DECLARE @kmRecorridos TKilometros
+	DECLARE @idVehiculo INT
+	SELECT @idViatico=idViatico, @kmRecorridos=kmRecorridos, @idVehiculo=idVehiculo FROM inserted
+	UPDATE Viatico SET monto= @kmRecorridos * (SELECT montoKm FROM Vehiculo WHERE id=@idVehiculo)
+	WHERE id=@idViatico
+GO
+
+--selects
+--SELECT * FROM Viatico
+--SELECT * FROM Vehiculo
+INSERT INTO Viatico(fecha, factura, monto, numPagos, notas, boleta, idTipoViatico, idProveedor, idResponsable, idEvento)
+VALUES ('2020-06-23', null, 0, 1, 'Pago de kilometraje2', 'KM-8493', 3, NULL, 2, 1)
+GO
+
+INSERT INTO Kilometraje(idViatico, kmRecorridos, idVehiculo)
+VALUES (5, 30, 1);
+GO
+
+-- 2. Trigger que valida que no se repita la boleta de un viático
+CREATE TRIGGER triggerBoleta ON Viatico
+AFTER INSERT, UPDATE
+AS
+	DECLARE @boleta TBoleta 
+	SELECT @boleta = boleta FROM inserted
+	IF EXISTS(SELECT id FROM Viatico WHERE boleta=@boleta)
+	BEGIN
+		PRINT 'Ya existe un viático con esta boleta'
+		ROLLBACK TRANSACTION
+	END
+GO
+
+INSERT INTO Viatico(fecha, factura, monto, numPagos, notas, boleta, idTipoViatico, idProveedor, idResponsable, idEvento)
+VALUES ('2020-06-26', '4532134792', 5000, 1, 'Comida', 'CO-5341', 1, 1, 3, 3)
+
